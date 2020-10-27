c4ebe2070a538dba44e596bd74d18aa0386f3676
Romeo Orsolino <rorsolino@robots.ox.ac.uk>
2020-10-27 16:15:09 +0000

    [EdgeDetectionRos] set publish frame to "odom"

--- edge_detection_ros/src/edge_detection_ros.cpp
+++ edge_detection_ros/src/edge_detection_ros.cpp
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "edge_detection_ros");
   ros::NodeHandle node_handle;
-  std::string frame_name = "point_cloud_odom";
+  std::string frame_name = "odom";
   edge_detection::EdgeDetectionRos edge_detection_ros(node_handle, frame_name, 0.4, 0.02);
   ros::MultiThreadedSpinner spinner(4);
   spinner.spin();
