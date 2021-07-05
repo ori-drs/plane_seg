147218ff3a556ef9b40da038d022d80f198f88e8
oliwiermelon <omelon@robots.ox.ac.uk>
2021-07-05 13:35:14 +0100

    [terrain_simplification_ros] load filter_chain within the ros node

--- terrain_simplification_ros/src/terrain_simplification_ros_node.cpp
+++ terrain_simplification_ros/src/terrain_simplification_ros_node.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "terrain_simplification_ros_node");
+  ros::init(argc, argv, "terrain_simplification");
   ros::NodeHandle node_handle("~");
   terrain_simplification_ros::TerrainSimplificationRos terrain_simplification_ros(node_handle);
   ros::AsyncSpinner spinner(2);
